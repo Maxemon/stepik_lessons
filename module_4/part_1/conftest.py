f4e073f7d11cedc0b8e6cefc31c47210745d9a81
Maxemon <maxemon@yandex.ru>
2021-06-05 23:55:17 +0300

    Additional Commit&Push для ДЗ №4 (часть 1). Добавил французский и испанский

--- module_4/part_1/conftest.py
+++ module_4/part_1/conftest.py
@@ -5,7 +5,7 @@ from selenium.webdriver.chrome.options import Options
 
 def pytest_addoption(parser):
     parser.addoption('--language', action='store', default="ru",
-                     help="Choose language: ru (default) or en-gb")
+                     help="Choose language: ru (default), en-GB, es, fr")
 
 
 @pytest.fixture(scope="function")
@@ -13,11 +13,11 @@ def browser(request):
     language = request.config.getoption("language")
     options = Options()
     options.add_experimental_option("prefs", {"intl.accept_languages": language})
-    if language == "ru" or language == "en-gb":
+    if language == "ru" or language == "en-GB" or language == "es" or language == "fr":
         print(f"\nstart chrome browser for test in {language} locale")
         browser = webdriver.Chrome(options=options)
     else:
-        raise pytest.UsageError("--language should be ru or en-gb")
+        raise pytest.UsageError("--language should be ru, en-GB, es, fr")
     yield browser
     print("\nquit browser..")
     browser.quit()
