2af73115480e93afb087fa84ca64e0b6da5aa7ae
Maxemon <maxemon@yandex.ru>
2021-06-05 23:41:36 +0300

    Additional Commit&Push для ДЗ №4 (часть 1)

--- module_4/part_1/test_items.py
+++ module_4/part_1/test_items.py
@@ -7,3 +7,5 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 def test_add_to_basket_button(browser):
     browser.get(link)
     assert exp.presence_of_element_located((By.CLASS_NAME, "btn-add-to-basket")), "No ""Add to basket"" button"
+    button_text = browser.find_element_by_class_name("btn-add-to-basket").text
+    assert button_text == "Добавить в корзину", "Text in button is incorrect"
