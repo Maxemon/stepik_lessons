81fe81e659755212b887e5374c4cb367bb62dcbf
Maxemon <maxemon@yandex.ru>
2021-06-10 21:05:23 +0300

    Final Commit&Push для ДЗ №4 (часть 1). Исправления после рецензии. Вынес локатор кнопки за тест - работает :) + добавил папку final

--- module_4/part_1/test_items.py
+++ module_4/part_1/test_items.py
@@ -1,8 +1,7 @@
 from selenium.webdriver.support import expected_conditions as exp
-from selenium.webdriver.common.by import By
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-button = By.CLASS_NAME, "btn-add-to-basket"
+button = ".btn-add-to-basket"
 expected_button_text = {
     "ru": "Добавить в корзину",
     "en-GB": "Add to basket",
@@ -17,5 +16,5 @@ def test_add_to_basket_button(browser, language):
 
     # Assert
     assert exp.presence_of_element_located(button), f"No '{expected_button_text[language]}' button"
-    button_text = browser.find_element(button).text
+    button_text = browser.find_element_by_css_selector(button).text
     assert button_text == expected_button_text[language], "Text in button is incorrect"
