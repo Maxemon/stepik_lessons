4116af06fc2a7904daae12094f5419922e4d544e
Maxemon <maxemon@yandex.ru>
2021-06-13 21:40:34 +0300

    Для страницы логина: Описал локаторы в locators.py Описал проверки в login_page.py
    Подгрузил в репозиторий __init__.py
    
    Подправил импорт в main_page.py

---
+++ module_5/pages/login_page.py
@@ -0,0 +1,25 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        assert "login" in self.browser.current_url
+
+    def should_be_login_form(self):
+        assert self.is_element_present(*LoginPageLocators.LOGIN_EMAIL), "No email field on login form"
+        assert self.is_element_present(*LoginPageLocators.LOGIN_PASSWORD), "No password field on login form"
+        assert self.is_element_present(*LoginPageLocators.LOGIN_BUTTON), "No login button on login form"
+
+    def should_be_register_form(self):
+        assert self.is_element_present(*LoginPageLocators.REGISTRATION_EMAIL), "No email field on registration form"
+        assert self.is_element_present(
+            *LoginPageLocators.REGISTRATION_PASSWORD), "No password field on registration form"
+        assert self.is_element_present(
+            *LoginPageLocators.REGISTRATION_PASSWORD_CONFIRM), "No password confirm field on registration form"
+        assert self.is_element_present(*LoginPageLocators.REGISTRATION_BUTTON), "No reg button on registration form"
